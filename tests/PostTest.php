bbe92670ee70962ef74d223c756c33b42881a46e
Jonathan Francis Roscoe <vcbnxn@alt42.com>
2014-09-15 16:30:34 +0100

    Add a test class for Post

---
+++ tests/PostTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* This PHPUnit test tests the public functions of model/Post.php
+* @author Jonathan Roscoe <vcbnxn@alt42.co.uk>
+*/
+
+class PostTest extends PHPUnit_Framework_TestCase
+{
+    public function testSetAndGetUser()
+    {
+	//create an instance of Post
+	
+	//set the User
+	
+	//Confirm getUser() returns the same value
+    }
+
+    public function testGetTimestamp()
+    {
+	//create an instance of Post
+	
+	//create am instance of PostManager
+	
+	//save Post
+	
+	//Confirm getTimestamp() returns a unix time stamp that matche's today's day/month/year
+    }    
+	
+	public function testSetAndGetContents()
+    {	
+	//create an instance of Post
+	
+	//set the Contents
+	
+	//Confirm getContents() returns the same value	
+    }
+}

e82a42d91e61bad175e019328eb393cc696078a3
Jonathan Francis Roscoe <vcbnxn@alt42.com>
2014-09-15 16:54:39 +0100

    Add implementation of PostModelTest.php

--- tests/PostModelTest.php
+++ tests/PostModelTest.php
@@ -9,31 +9,50 @@ class PostModelTest extends PHPUnit_Framework_TestCase
     public function testSetAndGetUser()
     {
 	//create an instance of Post
+	$post = new Post();
 	
 	//set the User
+	$post->setUser('Bob');
 	
 	//Confirm getUser() returns the same value
+	$this->assertEquals('Bob', $post->getUser());
     }
 
     public function testGetTimestamp()
     {
 	//create an instance of Post
+	$post = new Post();
 	
 	//create an instance of PostManager
+	$post_manager = new PostManager();
+	
+	//give post essentil values
+	$post->setUser('Bob');
+	$post->setContents('Hello World!');
 	
 	//Confirm getTimestamp() returns a null value before saved
+	assertEquals($post->getTimestamp(), null);
 	
 	//save Post
+	$post_manager->create($post);
 	
 	//Confirm getTimestamp() returns a unix time stamp that matche's today's day/month/year
+	$returned_timestamp = $post->getTimestamp();
+	
+	$current_timestamp = time();
+	
+	assertEquals(date('d-m-Y', $current_timestamp), date('d-m-Y', $timestamp_timestamp));
     }    
 	
 	public function testSetAndGetContents()
     {	
 	//create an instance of Post
+	$post = new Post();
 	
 	//set the Contents
+	$post->setContents('Hello World!')
 	
 	//Confirm getContents() returns the same value	
+	assertEquals($post->getContents(), 'Hello World!');
     }
 }

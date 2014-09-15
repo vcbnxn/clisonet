7f93142c2ad578b6e97d83eb40493e824ae1aad8
Jonathan Francis Roscoe <vcbnxn@alt42.com>
2014-09-15 15:26:39 +0100

    Add the entry point script and autoload file.

---
+++ src/autoload.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * autoload.php
+ * Bootstrapping for the social network demo.
+ * @author Jonathan Roscoe <vcbnxn@alt42.co.uk>
+ */
+ 
+ /**
+  * Autoloading function used by PHP in last-ditch attempt to load classes.
+  * Add a subdirectory check, just in case.
+  * @param string - the class name
+  */
+ function __autoload($class_name) {
+	$sub_dir = '';
+	
+	//use subdirectory if necessary
+	if (strpos($class_name, 'Model') ~= FALSE) {
+		$sub_dir = 'model/'
+	}elseif (strpos($class_name, 'Controller') ~= FALSE) {
+		$sub_dir = 'controllers/'
+	}elseif (strpos($class_name, 'View') ~= FALSE) {
+		$sub_dir = 'views/'
+	}
+	
+	$sub_dir = __FILE__ . $sub_dir; //use absolute path based on this file's location
+	
+    include_once $sub_dir . $class_name . '.php';
+}
+ 
+?>
---
+++ src/clisonet.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+/**
+ * clisonet.php
+ * This script insantiates a command line version of the social network demo. It is the main entry point of the program.
+ * @author Jonathan Roscoe <vcbnxn@alt42.co.uk>
+ */
+ 
+ require_once('autoload.php');
+ 
+?>
